3bfb046716eca4fd8476680320099619d9b345d5
mdodong <mdodong@gmail.com>
2018-10-09 19:44:49 +0800

    cleanup init file

--- sempy-backend/app/__init__.py
+++ sempy-backend/app/__init__.py
@@ -8,19 +8,15 @@ app = connexion.App(__name__, specification_dir='api/')
 
 
 # Read the swagger.yml file to configure the endpoints
-app.add_api('swagger.json')
+app.add_api('swagger.json', swagger_ui=True)
 
 # add CORS support
 CORS(app.app)
 
 # Create a URL route in our application for "/"
-flask_app = app.app
+#flask_app = app.app
 from app.explorer.routes import mod
-flask_app.register_blueprint(mod)
+app.app.register_blueprint(mod)
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
-    flask_app.run(debug=True)
-'''
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
-'''
+    app.run(debug=True)
