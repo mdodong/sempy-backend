73d4257ac2e170d5b683c9369253d869bf54f407
mdodong <mdodong@gmail.com>
2018-10-09 19:45:22 +0800

    change explorer and base template

--- sempy-backend/app/explorer/routes.py
+++ sempy-backend/app/explorer/routes.py
@@ -1,13 +1,12 @@
 import sys, os
 from flask import Blueprint, jsonify, render_template
-sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 import app.api
 
 mod = Blueprint('explorer', __name__)
 
 @mod.route('/')
-def home():
+def homepage():
     #return jsonify(api.get_delegates()) #
     #return '<h1>This is the Homepage</h1>'
-    return render_template('index.html')
+    return render_template('explorer.html')
