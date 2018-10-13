2b227a281cee7aede15beea16e39122ff2400586
mdodong <mdodong@gmail.com>
2018-10-13 11:04:17 +0800

    completed explorer functions for Tx, Address, Block and Delegates

--- sempy-backend/app/api/__init__.py
+++ sempy-backend/app/api/__init__.py
@@ -97,17 +97,75 @@ def delegates_by_address():
     delegates = get_delegates()['result']
     return build_dict(delegates, key="address")
 
+def process_delegate(delegate):
+    delegate["status"] = "Delegate"
+    if delegate["validator"]:
+        delegate["status"] = "Validator"
+    votes = float(delegate["votes"])/1000000000
+    delegate["votes"] = votes.is_integer() and int(votes) or votes
+    if int(delegate["turnsHit"]) == 0:
+        delegate["ratio"] = 0
+    else:
+        delegate["ratio"] = int(delegate["turnsHit"])/(int(delegate["turnsHit"]) + int(delegate["turnsMissed"])) * 100
+    #print(delegate["turnsHit"], delegate["turnsMissed"])
+    return delegate
+
+def process_all_delegates(all_delegates):
+    theDelegates = []
+    for d in range(len(all_delegates)):
+        theDelegates.append(process_delegate(all_delegates[d]))
+    return theDelegates
+
+
+def process_address(address):
+    delegates = delegates_by_address()
+    if address["address"] in delegates:
+        address["name"] = delegates.get(address["address"] )["name"]
+    available = float(address["available"])/1000000000
+    locked = float(address["locked"])/1000000000
+    address["available"] = available.is_integer() and int(available) or available
+    address["locked"] = locked.is_integer() and int(locked) or locked
+    address["totalBalance"] = address["available"] + address["locked"]
+    return address
+
+def process_transactions(tx):
+    delegates = delegates_by_address()
+    value = float(tx["value"])/1000000000
+    fee = float(tx["fee"])/1000000000
+    date = datetime.datetime.fromtimestamp(int(tx['timestamp'])/1000.0)
+    tx["value"] = value.is_integer() and int(value) or value
+    tx["fee"] = fee.is_integer() and int(fee) or fee
+    tx["timestamp"] = date.strftime('%Y-%m-%d %H:%M:%S')
+    if tx["from"] in delegates:
+        tx["fromName"] = delegates.get(tx["from"])['name']
+    if tx["to"] in delegates:
+        tx["toName"] = delegates.get(tx["to"])['name']
+    return tx
+
+def process_block(block):
+    delegates = delegates_by_address()
+    theBlock = block['result']
+    validatorName = delegates.get(theBlock['coinbase'])['name']
+    theBlock['validatorName'] = validatorName
+    txs = len(theBlock['transactions']) - 1
+    theBlock['transactionCount'] = txs
+    totalFees = 0
+    for x in range(txs+1):
+        theBlock['transactions'][x] = process_transactions(theBlock['transactions'][x])
+        totalFees += theBlock['transactions'][x]["fee"]
+    if totalFees == 0 or 0.0:
+        theBlock['totalFees'] = 0
+    else:
+        theBlock['totalFees'] = totalFees
+    date = datetime.datetime.fromtimestamp(int(theBlock['timestamp'])/1000.0)
+    theBlock['timestamp'] = date.strftime('%Y-%m-%d %H:%M:%S')
+    return theBlock
+
 def get_blocks(nBlocks=10):
     delegates = delegates_by_address()
-    latestBlockNumber = int(get_latest_block_number()['result'])
+    latestBlockNumber = int(get_latest_block_number()["result"])
     blocks = []
     for block in range(latestBlockNumber,(latestBlockNumber-nBlocks), -1):
-        theBlock = get_block_by_number(block)['result']
-        validatorName = delegates.get(theBlock['coinbase'])['name']
-        theBlock['coinbase'] = validatorName
-        txs = len(theBlock['transactions']) - 1
-        theBlock['transactions'] = txs
-        date = datetime.datetime.fromtimestamp(int(theBlock['timestamp'])/1000.0)
-        theBlock['timestamp'] = date = date.strftime('%Y-%m-%d %H:%M:%S')
+        theBlock = process_block(get_block_by_number(block))
         blocks.append(theBlock)
     return blocks
--- sempy-backend/app/explorer/routes.py
+++ sempy-backend/app/explorer/routes.py
@@ -7,9 +7,34 @@ mod = Blueprint('explorer', __name__)
 
 @mod.route('/')
 def homepage():
-    latestBlocks = api.get_blocks(20)
+    latestBlocks = api.get_blocks(15)
     return render_template('explorer.html', latestBlocks=latestBlocks)
 
 @mod.route('/explorer')
 def explorer():
     return homepage()
+
+@mod.route('/explorer/block/<int:number>')
+def block_by_number(number):
+    block = api.process_block(api.get_block_by_number(number))
+    return render_template('block.html', block=block)
+
+@mod.route('/explorer/block/<string:hash>')
+def block_by_hash(hash):
+    block = api.process_block(api.get_block_by_hash(hash))
+    return render_template('block.html', block=block)
+
+@mod.route('/delegates')
+def delegates():
+    delegates = api.process_all_delegates(api.get_delegates()["result"])
+    return render_template('delegates.html', delegates=delegates)
+
+@mod.route('/explorer/address/<string:address>')
+def semux_address(address):
+    address = api.process_address(api.get_account(address)["result"])
+    return render_template("address.html", address=address)
+
+@mod.route('/explorer/transaction/<string:hash>')
+def semux_transaction(hash):
+    transaction = api.process_transactions(api.get_transaction(hash)["result"])
+    return render_template("transaction.html", transaction=transaction)
